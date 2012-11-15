0f780621f4ee210fa5fbaadec073660a6d3041d2
Equan Pr <eq@junwatu.com>
2012-11-15 16:39:44 +0700

    using underscore templating

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 // server.js
 var express = require("express");
-var cons    = require('consolidate');
+var engines    = require('consolidate');
 var app = express();
 
 app.configure(function(){
 	app.use(express.logger());
+
 	app.use(express.static(__dirname + '/app'));
-	
-	app.engine('html',cons.underscore);
+
+	app.engine('html',engines.underscore);
+	/*
+	 Set views direcoctory. DO NOT set this with the static directory!. 
+	*/
+	app.set('views', __dirname+'/app/views');
 	app.set('view engine', 'html');
 
 	app.set('PORT',3000);
 });
 
 app.get("/",function(req,res){
-	res.render('index');
+	res.render('index', {title:'Express with Underscore!'});
 });
 
 app.listen(app.get('PORT'));
