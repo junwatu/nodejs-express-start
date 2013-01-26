985d814e8397cc414196c22b59f6ce1b996df317
Equan Pr <eq@junwatu.com>
2013-01-26 18:05:13 +0700

    detect env for production or development.

--- server.js
+++ server.js
@@ -1,26 +1,89 @@
-// server.js
-var express = require("express");
-var engines    = require('consolidate');
-var app = express();
+/*
+ * Simple Login Registration Node.js App
+ * This app use Express 3, Mongoose 3, Underscore and MongoDB database
+ *
+ *
+ * Author By Equan Pr.
+ * http://www.junwatu.com
+ *
+ * License :  Whatever you want! :D
+ */
 
-app.configure(function(){
-	app.use(express.logger());
+var express = require("express"),
+    engines = require('consolidate'),
+    app = express(),
+    url = require('url');
+    mongoose = require('mongoose'),
+    dbmessage = '';
 
-	app.use(express.static(__dirname + '/app'));
+app.configure(function () {
+    app.use(express.logger());
 
-	app.engine('html',engines.underscore);
-	/*
-	 Set views direcoctory. DO NOT set this with the static directory!. 
-	*/
-	app.set('views', __dirname+'/app/views');
-	app.set('view engine', 'html');
+    app.use(express.static(__dirname + '/app'));
 
-	app.set('PORT',3000);
+    app.engine('html', engines.underscore);
+
+    /*
+     Set views directory. DO NOT set this with the same static directory!.
+     */
+    app.set('views', __dirname + '/app/views');
+    app.set('view engine', 'html');
+    app.set('PORT', 3000);
+
+    // MongoDB for development
+    app.set('MONGODB_DEV','mongodb://localhost/contacts');
+
+    /**
+     * MongoDB for production
+     *
+     * HardCode for demo purpose only
+     */
+    app.set('MONGODB_PROD','mongodb://usernam:password@domainmu.com:212/contacts');
+
+    /**
+     * NODE_ENV=development
+     */
+    if ('development' == app.get('env')) {
+        app.set('MONGODB_CONN', app.get('MONGODB_DEV'));
+    }
+
+    /**
+     * NODE_ENV=production
+     */
+    if ('production' == app.get('env')) {
+        app.set('MONGODB_CONN', app.get('MONGODB_PROD'));
+    }
+
 });
 
-app.get("/",function(req,res){
-	res.render('index', {title:'Express with Underscore!'});
+/* ==================================
+ * MongoDB connection using Mongoose
+ */
+
+var db = mongoose.createConnection(app.get('MONGODB_CONN'));
+
+db.on('connected', function () {
+    console.log('Aplikasi terhubung dengan database.');
+    dbmessage = 'Aplikasi terhubung dengan database.';
+});
+
+db.on('error', function () {
+    console.error.bind(console, 'Connection error!');
+    dbmessage = 'Koneksi ke MongoDB error!';
+});
+
+//====================================
+
+app.get("/", function (req, res) {
+    res.render('index', {
+        title:'NodeStar',
+        description:dbmessage,
+        message:''
+    });
+
+    console.log(url.parse(req.url));
 });
 
 app.listen(app.get('PORT'));
-console.log("Server berjalan pada port "+app.get('PORT')+"...");
+console.log('NodeStar');
+console.log("Server Port: " + app.get('PORT'));
